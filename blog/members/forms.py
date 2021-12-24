9ed7f9d262ac58c6f2bfd0c19d647965be4080f8
hisham <hisham.sinan1@gmail.com>
2021-12-24 10:46:53 +0530

    Added Edit Profile Feature

--- blog/members/forms.py
+++ blog/members/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import User
 from django import forms
 
@@ -23,3 +23,28 @@ class SignUpForm(UserCreationForm):
         self.fields['password1'].widget.attrs['style'] = 'max-width: 300px;'
         self.fields['password2'].widget.attrs['class'] = 'form-control'
         self.fields['password2'].widget.attrs['style'] = 'max-width: 300px;'
+
+
+class EditProfileForm(UserChangeForm):
+    first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control',
+                                                                               'style': 'max-width: 300px;'}))
+    last_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control',
+                                                                              'style': 'max-width: 300px;'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control',
+                                                            'style': 'max-width: 300px;'}))
+    username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control',
+                                                                             'style': 'max-width: 300px;'}))
+    # last_login = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control',
+    #                                                                            'style': 'max-width: 300px;'}))
+    # is_superuser = forms.CharField(max_length=100, widget=forms.CheckboxInput(attrs={'class': 'form-check',
+    #                                                                                  'style': 'max-width: 300px;'}))
+    # is_staff = forms.CharField(max_length=100, widget=forms.CheckboxInput(attrs={'class': 'form-check',
+    #                                                                              'style': 'max-width: 300px;'}))
+    # is_active = forms.CharField(max_length=100, widget=forms.CheckboxInput(attrs={'class': 'form-check',
+    #                                                                               'style': 'max-width: 300px;'}))
+    # date_joined = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control',
+    #                                                                             'style': 'max-width: 300px;'}))
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'username', 'email')
--- blog/members/urls.py
+++ blog/members/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Registration
+from .views import Registration, UserEdit
 
 urlpatterns = [
-    path('register/', Registration.as_view(), name="register")
+    path('register/', Registration.as_view(), name="register"),
+    path('edit_profile/', UserEdit.as_view(), name="edit_profile"),
 ]
